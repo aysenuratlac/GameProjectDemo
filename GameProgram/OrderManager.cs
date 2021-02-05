3159ba8741ae3e63f68fcc7c03820f373db4f5dc
aysenuratlac <aysenuratlac@gmail.com>
2021-02-05 13:27:39 +0300

    GameProjectDemo

--- GameProgram/OrderManager.cs
+++ GameProgram/OrderManager.cs
@@ -16,6 +16,16 @@ namespace GameProgram
                 Console.WriteLine("İndirimli fiyat:" + game.gamePrice);
                 Console.WriteLine(gamer.FirtName + " , " + game.gameName + " satın aldınız. Keyifle oynamanız dileğiyle !");
             }
+
+            else if (game.gamePrice>=150)
+            {
+                game.gamePrice = game.gamePrice - 30;
+                Console.WriteLine("Kış Sonu İndirimi ----> -30TL");
+                Console.WriteLine("İndirimli fiyat:" + game.gamePrice);
+                Console.WriteLine(gamer.FirtName + " , " + game.gameName + " satın aldınız. Keyifle oynamanız dileğiyle !");
+
+
+            }
             
         }
 
